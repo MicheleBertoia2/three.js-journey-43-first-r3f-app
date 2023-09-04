22d2f974f2afc3717581fbdb47d4106f907702b0
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-09-04 16:15:17 +0200

    [first commit]

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import { Canvas } from '@react-three/fiber'
+import './style.css'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <>
+        
+        <Canvas>
+            <mesh>
+                <torusKnotGeometry />
+                <meshNormalMaterial/>
+            </mesh>
+        </Canvas>
+    </>
+)
