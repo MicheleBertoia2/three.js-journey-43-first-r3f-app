9415f41c343a1a071866597dc6cb86234c24b511
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-09-04 19:14:11 +0200

    [more exercise]

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,18 +1,51 @@
+import { extend, useFrame, useThree } from "@react-three/fiber"
+import { useRef } from "react"
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import CustomObject from "./CustomObject.jsx"
+
+extend({
+  OrbitControls 
+})
+
 export default function Experience()
 {
 
+  const {camera, gl} = useThree()
+  const cubeRef = useRef()
+  const groupRef =useRef()
+
+  
+
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta
+    // groupRef.current.rotation.y += delta
+  })
+
   return <>
-    <mesh scale={1.5} position={[2, 0, 0]} rotation-y={ Math.PI * 0.25 }>
-        <boxGeometry/>
-        <meshBasicMaterial color='darkOrchid' />
-    </mesh>
-    <mesh scale={1.5} position={[-2, 0, 0]} >
-        <sphereGeometry/>
-        <meshBasicMaterial color='orange' />
-    </mesh>
+
+    <orbitControls args={ [ camera, gl.domElement ] } />
+
+    <directionalLight position={ [1, 2, 3] } intensity={1.5}/>
+    <ambientLight intensity={0.5} />
+
+    <group ref={groupRef}>
+        <mesh ref={cubeRef} scale={1.5} position={[2, 0, 0]} rotation-y={ Math.PI * 0.25 }>
+          <boxGeometry/>
+          <meshStandardMaterial color='darkOrchid' />
+      </mesh>
+
+      <mesh scale={1.5} position={[-2, 0, 0]} >
+          <sphereGeometry/>
+          <meshStandardMaterial color='orange' />
+      </mesh>
+    </group>
+    
+
     <mesh position-y ={-2} rotation-x={ - Math.PI * 0.5 }  scale={10}>
       <planeGeometry/>
-      <meshBasicMaterial color={'green'}/>
+      <meshStandardMaterial color={'green'}/>
     </mesh>
+
+    <CustomObject />
   </>
 }
